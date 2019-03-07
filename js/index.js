51aeb490c5fb9162e1771cf5d6c4207bae71b832
github username <github email address>
2019-03-06 22:41:20 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
